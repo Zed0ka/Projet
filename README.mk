cc561dbcf781171e39450c99ce3d811c17159cad
Zed0ka <115066855+Zed0ka@users.noreply.github.com>
2022-10-18 17:20:35 +0200

    Création dossier projet

---
+++ README.mk
@@ -0,0 +1,24 @@
+Projet centré sur le JV - 
+
+Home : Cédric
+Un truc un peu stylé
+full flexbox
+mix images et txt
+menu burger / icônes nav interactives (boutons liens vers autres pages)
+
+Gallery : Brayan
+caroussel
+images, captures d'écran légendées
+catégories fail et win
+
+Contact : Brayan
+Formulaire
+nom prénom mail password+confirmation 
+pronom, genre, type de joueur, top 3 des jeux -> checkbox
+rang sur LoL, rang sur OW -> déroulants
+Boutons validation 
+
+Blog : Cédric
+Top 5 - jeux pc tryhard + une image + une case descriptive
+Top 5 - jeux pc casual + une image + une case descriptive
+
